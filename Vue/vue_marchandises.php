4ac89c718cfb679c1651cab5279e2d3e21939d67
ChristopHe <christophe.hervi@free.fr>
2020-11-18 17:41:32 +0100

    utilisation d'une ancre pour déplacerla page sur la marchandise sélectionnée

--- Vue/vue_marchandises.php
+++ Vue/vue_marchandises.php
@@ -12,10 +12,13 @@
 	$base = new base2donnees;
 	$T_ligne = $base->Marchandise();
 	for($i=0;$i<count($T_ligne);$i++) {
-		echo "\t",'<tr>',"\n";
+		echo "\t",($T_ligne[$i]['ID'] == $id_selectionné) ? '<tr  id="selection">' : '<tr>',"\n"; // pose d'une ancre sur la ligne
 		echo "\t\t",'<td>',$T_ligne[$i]['marchandise'],'</td>',"\n";
 		echo "\t\t",'<td>',$T_ligne[$i]['cours_ki'],'</td>',"\n";
 		echo "\t\t",'<td>',$T_ligne[$i]['cours_max'],'</td>',"\n";
+		if ($T_ligne[$i]['ID'] == $id_selectionné) {
+			echo  "\t",'<tr><td colspan="3">Marchandise s&eacute;lectionn&eacute;e</td></tr>',"\n";
+		}
 		echo "\t",'</tr>',"\n";
 	}
 ?>
--- marchandises.php
+++ marchandises.php
@@ -1,6 +1,16 @@
 <?php
 session_start(); // On démarre la session
 require 'Modele/classe_BD.php';
+
+$liste_ordre = array (
+//	ordre => script. Pour ajouter un nouvel ordre il suffira d'ajouter une ligne
+	'detail' => 'detail_marchandise',
+	'MAJ'	 => 'MAJ_prix'
+);
+
+// ID de la marchandise
+$id_selectionné = (int) $_GET['id'];
+
 define(ONGLET_ACTIF, 2);
 
-include('Vue/doctype.html');
+include 'Vue/doctype.html';
