27d3a413918b0b0360180890f53dc74466a7e996
ChristopHe <christophe.hervi@free.fr>
2020-11-18 15:02:23 +0100

    modification de l'indentation dans le code généré

--- Vue/marchandises.php
+++ Vue/marchandises.php
@@ -1,11 +1,11 @@
 <h1>Page marchandises en construction</h1>
 	<table>
 	<thead>
-		<tr>
-			<th>Marchandise</th>
-			<th>cours Ki-market</th>
-			<th>cours max</th>
-		</tr>
+	<tr>
+		<th>Marchandise</th>
+		<th>cours Ki-market</th>
+		<th>cours max</th>
+	</tr>
 	</thead>
 	<tbody>
 <?php
@@ -13,11 +13,11 @@
 	$base = new base2donnees;
 	$T_ligne = $base->Marchandise();
 	for($i=0;$i<count($T_ligne);$i++) {
-		echo "\t\t",'<tr>',"\n";
-		echo "\t\t\t",'<td>',$T_ligne[$i]['marchandise'],'</td>',"\n";
-		echo "\t\t\t",'<td>',$T_ligne[$i]['cours_ki'],'</td>',"\n";
-		echo "\t\t\t",'<td>',$T_ligne[$i]['cours_max'],'</td>',"\n";
-		echo "\t\t",'</tr>',"\n";
+		echo "\t",'<tr>',"\n";
+		echo "\t\t",'<td>',$T_ligne[$i]['marchandise'],'</td>',"\n";
+		echo "\t\t",'<td>',$T_ligne[$i]['cours_ki'],'</td>',"\n";
+		echo "\t\t",'<td>',$T_ligne[$i]['cours_max'],'</td>',"\n";
+		echo "\t",'</tr>',"\n";
 	}
 ?>
 	</tbody>
