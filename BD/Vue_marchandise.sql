01210eaba3684a3e4b773ed6c61daa9e9cc6fca0
ChristopHe <christophe.hervi@free.fr>
2020-11-19 12:05:48 +0100

    réduction à un seul paramètre: id

--- BD/Vue_marchandise.sql
+++ BD/Vue_marchandise.sql
@@ -1,7 +1,7 @@
 CREATE VIEW Vue_marchandise AS
 SELECT 
 	ID,
-	CONCAT('<a href="?ordre=detail&id=',ID,'#selection"><img src="Vue/images/',image, '.png" alt ="',nom,'">',nom,'</a>') AS marchandise,
+	CONCAT('<a href="?id=',ID,'#selection"><img src="Vue/images/',image, '.png" alt ="',nom,'">',nom,'</a>') AS marchandise,
 	CONCAT(cours_ki,'&euro;') AS cours_ki,
 	CONCAT(cours_ki,'&euro;') AS cours_max
 FROM marchandise WHERE nature_ID BETWEEN 1 AND 2
--- marchandises.php
+++ marchandises.php
@@ -2,12 +2,6 @@
 session_start(); // On démarre la session
 require 'Modele/classe_BD.php';
 
-$liste_ordre = array (
-//	ordre => script. Pour ajouter un nouvel ordre il suffira d'ajouter une ligne
-	'detail' => 'detail_marchandise',
-	'MAJ'	 => 'MAJ_prix'
-);
-
 // ID de la marchandise
 $id_selectionné = (int) $_GET['id'];
 
