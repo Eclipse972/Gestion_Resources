6b5f0c728530d286b67b74a58d519db964df76f0
ChristopHe <christophe.hervi@free.fr>
2020-11-19 15:17:48 +0100

    formatage monétaire

--- BD/Vue_marchandise.sql
+++ BD/Vue_marchandise.sql
@@ -2,6 +2,6 @@ CREATE VIEW Vue_marchandise AS
 SELECT 
 	ID,
 	CONCAT('<a href="?id=',ID,'#selection"><img src="Vue/images/',image, '.png" alt ="',nom,'">',nom,'</a>') AS marchandise,
-	CONCAT(cours_ki,'&euro;') AS cours_ki,
-	CONCAT(cours_ki,'&euro;') AS cours_max
+	cours_ki,
+	cours_max
 FROM marchandise WHERE nature_ID BETWEEN 1 AND 2
--- Vue/vue_marchandises.php
+++ Vue/vue_marchandises.php
@@ -14,8 +14,8 @@
 	for($i=0;$i<count($T_ligne);$i++) {
 		echo "\t",($T_ligne[$i]['ID'] == $id_selectionné) ? '<tr id="selection">' : '<tr>',"\n"; // pose d'une ancre sur la ligne sélectionnée
 		echo "\t\t",'<td>',$T_ligne[$i]['marchandise'],'</td>',"\n";
-		echo "\t\t",'<td>',$T_ligne[$i]['cours_ki'],'</td>',"\n";
-		echo "\t\t",'<td>',$T_ligne[$i]['cours_max'],'</td>',"\n";
+		echo "\t\t",'<td>',number_format($T_ligne[$i]['cours_ki'],0, ',', ' '),'&euro;</td>',"\n";
+		echo "\t\t",'<td>',number_format($T_ligne[$i]['cours_max'],0, ',', ' '),'&euro;</td>',"\n";
 		if ($T_ligne[$i]['ID'] == $id_selectionné) {
 			echo  "\t",'<tr><td colspan="3">Marchandise s&eacute;lectionn&eacute;e</td></tr>',"\n";
 		}
