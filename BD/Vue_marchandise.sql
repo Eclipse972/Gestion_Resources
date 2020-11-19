d79d363f122fa6eeb42e8d933c172c5601b6f52c
ChristopHe <christophe.hervi@free.fr>
2020-11-19 11:59:52 +0100

    suppression des liens sur les prix

--- BD/Vue_marchandise.sql
+++ BD/Vue_marchandise.sql
@@ -2,6 +2,6 @@ CREATE VIEW Vue_marchandise AS
 SELECT 
 	ID,
 	CONCAT('<a href="?ordre=detail&id=',ID,'#selection"><img src="Vue/images/',image, '.png" alt ="',nom,'">',nom,'</a>') AS marchandise,
-	CONCAT('<a href="?ordre=MAJ&id=',ID,'#selection">',cours_ki,'€</a>') AS cours_ki,
-	CONCAT('<a href="?ordre=MAJ&id=',ID,'#selection">',cours_max,'€</a>') AS cours_max
+	CONCAT(cours_ki,'&euro;') AS cours_ki,
+	CONCAT(cours_ki,'&euro;') AS cours_max
 FROM marchandise WHERE nature_ID BETWEEN 1 AND 2
