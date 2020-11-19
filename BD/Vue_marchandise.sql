a7aac1783ffd0ffdc19cb1a9d1cad374906cddd9
ChristopHe <christophe.hervi@free.fr>
2020-11-20 00:30:19 +0100

    formatage des nombres

--- BD/Vue_marchandise.sql
+++ BD/Vue_marchandise.sql
@@ -2,6 +2,6 @@ CREATE VIEW Vue_marchandise AS
 SELECT 
 	ID,
 	CONCAT('<a href="?id=',ID,'#selection"><img src="Vue/images/',image, '.png" alt ="',nom,'">',nom,'</a>') AS marchandise,
-	cours_ki,
-	cours_max
+	CONCAT(REPLACE(FORMAT(cours_ki ,0),',', ' '),'&euro;') AS cours_ki,
+	CONCAT(REPLACE(FORMAT(cours_max ,0),',', ' '),'&euro;') AS cours_max
 FROM marchandise WHERE nature_ID BETWEEN 1 AND 2
