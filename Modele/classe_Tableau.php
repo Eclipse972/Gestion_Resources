e0b0faddd277a731fb2a2b6eafb1d2b434cd5c3c
ChristopHe <christophe.hervi@free.fr>
2020-11-20 22:09:25 +0100

    suppression de paramètres inutiles et leurs conséquences

--- Modele/classe_Tableau.php
+++ Modele/classe_Tableau.php
@@ -1,9 +1,5 @@
 <?php
 class Tableau {
-protected $Vue_BD;	// vue associé à la table. Elle prépare les données de la table pour l'affichage
-protected $T_en_tete;	// tableau contenant les en-têtes du tableau à afficher
-// dans le futur protected $Table_BD;	// table pour la MAJ des données
-
 public function __construct($date) {
 ?>
 <p align="right">Derni&egrave;re mise à jour le: <?=$date?></p>
@@ -11,54 +7,63 @@ public function __construct($date) {
 <?php
 }
 
-public function Afficher_tete() { // déclare le tableau et affiche l'en-tête
+protected function Afficher_tete($T_en_tete) { // déclare le tableau avec en paramètres un tableau contenant les en-têtes à afficher
 ?>
 	<thead>
 	<tr>
-<?php	for($i=0;$i<count($this->T_en_tete);$i++) {	echo "\t\t",'<th>',$this->T_en_tete[$i],'</th>',"\n";	}	?>
+<?php	for($i=0;$i<count($T_en_tete);$i++) {	echo "\t\t",'<th>',$T_en_tete[$i],'</th>',"\n";	}	?>
 	</tr>
 	</thead>
 <?php
 }
 	
-protected function Afficher_corps($id_selectionné) {
+protected function Afficher_corps($Vue_BD, $nb_col_rapport, $id_selectionné) {
 ?>
 	<tbody>
 <?php
 	$BD = new base2donnees;
-	$T_Vue = $BD->Récupère_Vue($this->Vue_BD); // récupère la vue
+	$T_Vue = $BD->Récupère_Vue($Vue_BD); // récupère la vue
 	
 	for($i=0;$i<count($T_Vue);$i++) {
 		echo "\t",($T_Vue[$i]['ID'] == $id_selectionné) ? '<tr id="selection">' : '<tr>',"\n"; // pose d'une ancre sur la ligne sélectionnée
 		echo $T_Vue[$i]['code'],"\t";
 		echo '</tr>',"\n";
-		if ($T_Vue[$i]['ID'] == $id_selectionné) $this->Afficher_rapport($id_selectionné,$T_Vue[$i]['nom_ligne']); // le 2e paramètre permet de récupérer le nom sans refaire une requête
+		if ($T_Vue[$i]['ID'] == $id_selectionné) {
+			$this->Début_rapport($nb_col_rapport);
+			$this->Afficher_rapport($id_selectionné,$T_Vue[$i]['nom_ligne']); // le 2e paramètre permet de récupérer le nom sans refaire une requête
+			$this->Fin_rapport();
+		}
 	}
 ?>
 	</tbody>
 	</table>
 <?php
 }
-}
-/////////////////////////////////////////////////////////////////////////
-class TMarchandise extends Tableau {
-public function __construct($date) {
-	$this->Vue_BD = 'Vue_marchandise';
-	$this->T_en_tete = array('Marchandise', 'cours Ki-market',	'cours max');
-	parent::__construct($date);
-}
 
-//public function Afficher_tete() { parent::Afficher_tete(); }
-public function Afficher_corps($id_selectionné) { parent::Afficher_corps($id_selectionné); }
-
-public function Afficher_rapport($id, $nom_ligne) {
-?>
-	<tr>
-		<td colspan="<?=count($this->T_en_tete)?>" id="rapport">
-		<p>Rapport <?=$nom_ligne?> en construction</p>
-		</td>
+protected function Début_rapport($nb_col_rapport) {
+?>	<tr>
+		<td colspan="<?=$nb_col_rapport?>" id="rapport">
+<?php
+}
+protected function Fin_rapport() {
+?>		</td>
 	</tr>
 <?php	
 }
 }
+// Les classes fille utilisées dans les onglets
+class TMarchandise extends Tableau {
+public function __construct($date) { parent::__construct($date); }
 
+public function Afficher_tete() { parent::Afficher_tete(array('Marchandise', 'cours Ki-market',	'cours max')); }
+
+public function Afficher_corps($Vue,$nb_col_rapport,$id_selectionné) { parent::Afficher_corps($Vue, $nb_col_rapport, $id_selectionné); }
+
+public function Afficher_rapport($id, $nom_ligne) {
+?>		<p>Rapport <?=$nom_ligne?> en construction</p>
+<?php
+}
+} // fin de classe TMarchandise
+
+class TMine extends Tableau {
+} // fin de clase TMine
--- Vue/vue_marchandises.php
+++ Vue/vue_marchandises.php
@@ -1,4 +1,4 @@
 <?php
 $Tableau = new TMarchandise('ind&eacute;termin&eacute;e');
 $Tableau->Afficher_tete();
-$Tableau->Afficher_corps($id_selectionné);
+$Tableau->Afficher_corps('Vue_marchandise',3,$id_selectionné); // paramètre id_selectionné défini dans marchandise.php
