331010cfcda9f2e404fef096ebe9f09cb2ba3709
ChristopHe <christophe.hervi@free.fr>
2020-11-21 03:37:27 +0100

    création de la classe usine

--- BD/Vue_usine.sql
+++ BD/Vue_usine.sql
@@ -1,10 +1,12 @@
 CREATE VIEW Vue_usine AS
 SELECT 
-	ID,
+	type_usine.ID,
 	CONCAT(
-		'\t\t<td><a href="?id=',ID,'#selection"><img src="Vue/images/',image, '.png" alt ="',nom,'">',nom,'</a></td>\n',
+		'\t\t<td><a href="?id=',type_usine.ID,'#selection"><img src="Vue/images/',type_usine.image, '.png" alt ="',type_usine.nom,'">',type_usine.nom,'</a></td>\n',
 		'\t\t<td>',REPLACE(FORMAT(0 ,0),',', ' '),'</td>\n',
-		'\t\t<td>',REPLACE(FORMAT(0 ,0),',', ' '),'</td>\n'
+		'\t\t<td>',REPLACE(FORMAT(0 ,0),',', ' '),' ',unites.nom,'/h</td>\n'
 	) AS code,
-	nom AS nom_ligne
-FROM type_usine
+	type_usine.nom AS nom_ligne
+FROM type_usine, marchandise, unites
+WHERE type_usine.marchandise_ID = marchandise.ID
+AND marchandise.unité_ID = unites.ID
