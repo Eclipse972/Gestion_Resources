23b2e0374beab61d223802989df0b37f7d5d1eda
ChristopHe <christophe.hervi@free.fr>
2020-11-21 06:47:28 +0100

    simplication de code

--- Modele/classe_Tableau.php
+++ Modele/classe_Tableau.php
@@ -12,7 +12,7 @@ protected function Afficher_tete($T_en_tete) { // déclare le tableau avec en pa
 	<thead>
 	<tr>
 		<td><!-- colonne pour l'image --></td>
-<?php	for($i=0;$i<count($T_en_tete);$i++) {	echo "\t\t",'<th>',$T_en_tete[$i],'</th>',"\n";	}	?>
+<?php	foreach($T_en_tete as $valeur) {	echo "\t\t",'<th>',$valeur,'</th>',"\n";	}	?>
 	</tr>
 	</thead>
 <?php
@@ -25,13 +25,13 @@ protected function Afficher_corps($Vue_BD, $id_selectionné) {
 	$BD = new base2donnees;
 	$T_Vue = $BD->Récupère_Vue($Vue_BD); // récupère la vue
 	
-	for($i=0;$i<count($T_Vue);$i++) {
-		echo "\t",($T_Vue[$i]['ID'] == $id_selectionné) ? '<tr id="selection">' : '<tr>',"\n"; // pose d'une ancre sur la ligne sélectionnée
-		echo $T_Vue[$i]['code'],"\t";
+	foreach($T_Vue as $valeur) {
+		echo "\t",($valeur['ID'] == $id_selectionné) ? '<tr id="selection">' : '<tr>',"\n"; // pose d'une ancre sur la ligne sélectionnée
+		echo $valeur['code'],"\t";
 		echo '</tr>',"\n";
-		if ($T_Vue[$i]['ID'] == $id_selectionné) {
+		if ($valeur['ID'] == $id_selectionné) {
 			$this->Début_rapport($this->nb_col_tableau);
-			$this->Afficher_rapport($id_selectionné,$T_Vue[$i]['nom_ligne']); // le 2e paramètre permet de récupérer le nom sans refaire une requête
+			$this->Afficher_rapport($id_selectionné,$valeur['nom_ligne']); // le 2e paramètre permet de récupérer le nom sans refaire une requête
 			$this->Fin_rapport();
 		}
 	}
@@ -98,7 +98,7 @@ public function Afficher_rapport($id, $nom_ligne) {
 // classe TEntrepot -------------------------------------------------------------------------------------------------------------------
 class TEntrepot extends Tableau {
 
-public function Afficher_tete() { parent::Afficher_tete(array('Usine', 'Niveau', 'Stock')); }
+public function Afficher_tete() { parent::Afficher_tete(array('Entrep&ocirc;t', 'Niveau', 'Stock')); }
 
 public function Afficher_corps($id_selectionné) { parent::Afficher_corps('Vue_entrepot', $id_selectionné); }
 
