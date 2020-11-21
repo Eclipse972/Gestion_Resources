700514493c75ac56c16ca618db09ee10ed1cd06a
ChristopHe <christophe.hervi@free.fr>
2020-11-21 13:14:30 +0100

    nature des recettes

---
+++ BD/nature_recette.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.5
+-- http://www.phpmyadmin.net
+--
+-- Serveur: gestion.resources.sql.free.fr
+-- Généré le : Sam 21 Novembre 2020 à 10:36
+-- Version du serveur: 5.0.83
+-- Version de PHP: 5.3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `gestion_resources`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nature_recette`
+--
+
+CREATE TABLE IF NOT EXISTS `nature_recette` (
+  `ID` int(11) NOT NULL auto_increment,
+  `NOM` varchar(30) collate latin1_general_ci NOT NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `nature_recette`
+--
+
+INSERT INTO `nature_recette` (`ID`, `NOM`) VALUES
+(1, 'production'),
+(2, 'amélioration b&acirc;timent'),
+(3, 'recrutement');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
