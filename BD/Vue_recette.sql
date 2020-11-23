0ffcd4bfd52281a6386ea5c0d336ef5306fd3539
ChristopHe <christophe.hervi@free.fr>
2020-11-23 01:34:14 +0100

    ajout d'un commentaire

--- BD/Vue_recette.sql
+++ BD/Vue_recette.sql
@@ -1,4 +1,5 @@
-# Pépare la lise des ingédints pour chaque recette
+-- Vue pour l'affichage des recettes
+-- /!\ dans l'affiche le code des image dans Phpmyadmin est remplacé par le balise alt de chaque image
 CREATE VIEW Vue_recette AS
 SELECT 
 	ingredients.recette_ID AS ID,
@@ -6,37 +7,9 @@ SELECT
 		ABS(ingredients.quantité),
 		'\t<img src="Vue/images/',marchandise.image,'.png" alt ="',marchandise.nom,'">',
 		IF(ingredients.nature = 0,' + ',IF(ingredients.nature =1,' -> ',''))
-	SEPARATOR '') as code
-FROM ingredients, marchandise
-WHERE ingredients.marchandise_ID = marchandise.ID
-ORDER BY ingredients.nature ASC
-GROUP BY ingredients.recette_ID;
-
-# ordonnner les ingrédients
-SELECT 
-	ingredients.recette_ID AS ID,
-	CONCAT(
-		ABS(ingredients.quantité),
-		'\t<img src="Vue/images/',marchandise.image,'.png" alt ="',marchandise.nom,'">',
-		IF(ingredients.nature = 0,' + ',IF(ingredients.nature =1,' -> ',''))
+		ORDER BY ingredients.nature ASC SEPARATOR ''
 	) as code
 FROM ingredients, marchandise
 WHERE ingredients.marchandise_ID = marchandise.ID
-ORDER BY ingredients.recette_ID ASC,ingredients.nature ASC;
+GROUP BY ingredients.recette_ID
 
-# imbrication
-SELECT ID, GROUP_CONCAT(code, SEPARATOR '') AS code
-FROM (-- table des ingrédients préalablement triée correctement
-	SELECT 
-		ingredients.recette_ID AS ID,
-		CONCAT(
-			ABS(ingredients.quantité),
-			'\t<img src="Vue/images/',marchandise.image,'.png" alt ="',marchandise.nom,'">',
-			IF(ingredients.nature = 0,' + ',IF(ingredients.nature =1,' -> ',''))
-		) as code
-	FROM ingredients, marchandise
-	WHERE ingredients.marchandise_ID = marchandise.ID
-	ORDER BY ingredients.recette_ID ASC,ingredients.nature ASC
-) AS preparation
-GROUP BY ID
-ORDER BY ID DESC
