17c444598321236650cd77014730dc0f3aaa4996
ChristopHe <christophe.hervi@free.fr>
2020-11-23 04:44:28 +0100

    nettoyage

--- Controleur/rapport_machandise.php
+++
@@ -1,5 +0,0 @@
-<?php
-function Rapport_marchandise($id) { // génère le code du détail d'une marchandise spécifiée
-	$code = 'Rapport marchandise en construction';
-	return $code;
-}
--- Controleur/session_start.php
+++
@@ -1,3 +0,0 @@
-<?php
-require 'Modele/classe_BD.php';
-
