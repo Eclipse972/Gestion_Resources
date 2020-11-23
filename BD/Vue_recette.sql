79a0ef9c55ff8a6be3296f8df66b969f3595c228
ChristopHe <christophe.hervi@free.fr>
2020-11-23 01:44:18 +0100

    modification du commentaire

--- BD/Vue_recette.sql
+++ BD/Vue_recette.sql
@@ -1,5 +1,5 @@
 -- Vue pour l'affichage des recettes
--- /!\ dans l'affiche le code des image dans Phpmyadmin est remplacé par le balise alt de chaque image
+-- /!\ dans l'affichage le code des image dans Phpmyadmin est remplacé par le balise alt de chaque image. Le code est nettement plus lisible mais c'est surpremnant.
 CREATE VIEW Vue_recette AS
 SELECT 
 	ingredients.recette_ID AS ID,
