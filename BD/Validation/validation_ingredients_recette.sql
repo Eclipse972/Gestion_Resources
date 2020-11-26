b381872fb90218118c54fe4b7dbccb98e1c5f07a
ChristopHe <christophe.hervi@free.fr>
2020-11-26 07:17:34 +0100

    écriture correcte de jointure + correction d'un champ

--- BD/Validation/validation_ingredients_recette.sql
+++ BD/Validation/validation_ingredients_recette.sql
@@ -4,12 +4,11 @@ SELECT
 	nature_recette.nom AS nature,
 	recette.nom AS recette,
 	recette.ID AS ID_recette,
-	IF(marchandise_ID = 1,'cout de production',IF(ingredients.quantité >0,'produit','ingédient')) AS type,
+	IF(marchandise_ID = 1,'cout',IF(ingredients.quantité >0,'produit','ingédient')) AS type,
 	marchandise.nom AS résultat,
 	ingredients.quantité
-FROM recette, ingredients, marchandise, nature_recette
-WHERE
-	ingredients.recette_ID = recette.ID			# jointure
-AND	ingredients.marchandise_ID = marchandise.ID	# jointure
-AND recette.nature_ID = nature_recette.ID		# jointure
+FROM recette
+INNER JOIN ingredients ON ingredients.recette_ID = recette.ID
+INNER JOIN marchandise ON ingredients.marchandise_ID = marchandise.ID
+INNER JOIN nature_recette ON recette.nature_ID = nature_recette.ID
 ORDER BY ingredients.recette_ID ASC, ingredients.nature DESC
