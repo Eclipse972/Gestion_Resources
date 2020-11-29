a9a1bbfd33619679c036186c618080c1dc7a200d
ChristopHe <christophe.hervi@free.fr>
2020-11-29 01:59:21 +0100

    erreur de nom

--- Modele/classe_BD_marchandises.php
+++ Modele/classe_BD_marchandises.php
@@ -1,7 +1,7 @@
 <?php
 require'Modele/classe_BD.php';
 
-class BD_marchandise extends base2donnees {
+class BD_marchandises extends base2donnees {
 
 // méthodes nécessaires pour l'affichage du tableau	
 
@@ -16,4 +16,5 @@ public function MarchandiseAbesoin($IDmarchandise) {
 	$this->resultat = $this->BD->query('SELECT code FROM Vue_marchandiseAbesoin WHERE ID = '.$IDmarchandise);
 	$ligne = $this->resultat->fetch(); // un seul résultat
 	return (isset($ligne['code'])) ? "<ul>\n".$ligne['code']."\t</ul>\n" : "<p>Rien</p>\n";	
+}
 }
