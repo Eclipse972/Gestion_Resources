5d82085d469e2801a0d286bae4656581460b5672
ChristopHe <christophe.hervi@free.fr>
2020-12-03 21:46:44 +0100

    remplacement de Vue_recette_usine par une requête imbriquée

--- BD/Vue_recette_usine.sql
+++
@@ -1,5 +0,0 @@
--- permet d'associer l'usine à sa recette
-CREATE VIEW Vue_recette_usine AS
-SELECT type_usine.ID, Vue_recette.code
-FROM Vue_recette
-INNER JOIN  type_usine ON type_usine.production_ID = Vue_recette.ID
--- BD/Vue_usine.sql
+++ BD/Vue_usine.sql
@@ -5,7 +5,16 @@ SELECT
 	CONCAT(
 		'\t\t<td><p id="gauche"><a href="?id=',type_usine.ID,'#selection"><img src="Vue/images/',type_usine.image, '.png" alt ="',type_usine.nom,'"></a></p>\n',
 		'\t\t\t<a href="?id=',type_usine.ID,'#selection"><h1>',UCASE(LEFT(type_usine.nom,1)),SUBSTRING(type_usine.nom,2,LENGTH(type_usine.nom)),'</h1></a>\n\t\t\t<p id="petite_image">',
-		Vue_recette.code,
+		(SELECT GROUP_CONCAT( # requête qui crée la recette
+					ABS(ingredient.quantité),
+					'\t<img src="Vue/images/',marchandise.image,'.png" alt ="',marchandise.nom,'">',
+					IF(ingredient.nature = 0,' + ',IF(ingredient.nature =1,' -> ',''))
+					ORDER BY ingredient.nature ASC SEPARATOR ''
+				) AS code
+			FROM ingredient
+			INNER JOIN marchandise ON ingredient.marchandise_ID = marchandise.ID
+			WHERE ingredient.recette_ID = type_usine.production_ID
+		),
 		'</p>\n\t\t</td>\n\t\t<td>',usine.niveau,'</td>\n',
 		'\t\t<td>',type_usine.prod_niveau1*usine.niveau,' ',unites.nom,'/h</td>\n'
 	) AS code,
@@ -14,4 +23,3 @@ FROM usine
 INNER JOIN type_usine ON usine.type_usine_ID = type_usine.ID
 INNER JOIN marchandise ON type_usine.marchandise_ID = marchandise.ID
 INNER JOIN unites ON marchandise.unité_ID = unites.ID
-INNER JOIN Vue_recette ON type_usine.production_ID = Vue_recette.ID
