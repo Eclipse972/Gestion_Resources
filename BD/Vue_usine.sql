dc90f11b8d62070e91a5ec202775abe9605df757
ChristopHe <christophe.hervi@free.fr>
2020-12-03 21:18:13 +0100

    isoler Vue_recette dans la requête

--- BD/Vue_usine.sql
+++ BD/Vue_usine.sql
@@ -4,9 +4,9 @@ SELECT
 	usine.joueur_ID AS IDjoueur,
 	CONCAT(
 		'\t\t<td><p id="gauche"><a href="?id=',type_usine.ID,'#selection"><img src="Vue/images/',type_usine.image, '.png" alt ="',type_usine.nom,'"></a></p>\n',
-		'\t\t\t<a href="?id=',type_usine.ID,'#selection"><h1>',UCASE(LEFT(type_usine.nom,1)),SUBSTRING(type_usine.nom,2,LENGTH(type_usine.nom)),'</h1></a>\n',
-		'\t\t\t<p id="petite_image">',Vue_recette.code,'</p>\n\t\t</td>\n',
-		'\t\t<td>',usine.niveau,'</td>\n',
+		'\t\t\t<a href="?id=',type_usine.ID,'#selection"><h1>',UCASE(LEFT(type_usine.nom,1)),SUBSTRING(type_usine.nom,2,LENGTH(type_usine.nom)),'</h1></a>\n\t\t\t<p id="petite_image">',
+		Vue_recette.code,
+		'</p>\n\t\t</td>\n\t\t<td>',usine.niveau,'</td>\n',
 		'\t\t<td>',type_usine.prod_niveau1*usine.niveau,' ',unites.nom,'/h</td>\n'
 	) AS code,
 	type_usine.nom AS nom_ligne
