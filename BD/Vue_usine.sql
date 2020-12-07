f22588f0db276884607b4f17a7050b1d65bef0bc
ChristopHe <christophe.hervi@free.fr>
2020-12-08 00:50:19 +0100

    séparation des milliers pour les nombres pour l'onglet usine

--- BD/Vue_usine.sql
+++ BD/Vue_usine.sql
@@ -3,8 +3,8 @@ SELECT
 	type_usine.ID,
 	usine.joueur_ID AS IDjoueur,
 	CONCAT(
-		'\t\t<td><p id="gauche"><a href="?id=',type_usine.ID,'#selection"><img src="Vue/images/',type_usine.image, '.png" alt ="',type_usine.nom,'"></a></p>\n',
-		'\t\t\t<a href="?id=',type_usine.ID,'#selection"><h1>',UCASE(LEFT(type_usine.nom,1)),SUBSTRING(type_usine.nom,2,LENGTH(type_usine.nom)),'</h1></a>\n\t\t\t<p id="petite_image">',
+		'\t\t<td><p id="gauche"><a href="?id=',CAST(type_usine.ID AS CHAR),'#selection"><img src="Vue/images/',type_usine.image, '.png" alt ="',type_usine.nom,'"></a></p>\n',
+		'\t\t\t<a href="?id=',CAST(type_usine.ID AS CHAR),'#selection"><h1>',UCASE(LEFT(type_usine.nom,1)),SUBSTRING(type_usine.nom,2,LENGTH(type_usine.nom)),'</h1></a>\n\t\t\t<p id="petite_image">',
 		(SELECT GROUP_CONCAT( # requête qui crée la recette
 					ABS(ingredient.quantité),
 					'\t<img src="Vue/images/',marchandise.image,'.png" alt ="',marchandise.nom,'">',
@@ -15,8 +15,8 @@ SELECT
 			INNER JOIN marchandise ON ingredient.marchandise_ID = marchandise.ID
 			WHERE ingredient.recette_ID = type_usine.production_ID
 		),
-		'</p>\n\t\t</td>\n\t\t<td>',usine.niveau,'</td>\n',
-		'\t\t<td>',type_usine.prod_niveau1*usine.niveau,' ',unites.nom,'/h</td>\n'
+		'</p>\n\t\t</td>\n\t\t<td>',CAST(usine.niveau AS CHAR),'</td>\n',
+		'\t\t<td>',REPLACE(CAST(FORMAT(type_usine.prod_niveau1*usine.niveau,0) AS CHAR),',',' '),' ',unites.nom,'/h</td>\n'
 	) AS code,
 	type_usine.nom AS nom_ligne
 FROM usine
