5db4be0c9b181c8037523441553f484867427f22
ChristopHe <christophe.hervi@free.fr>
2020-12-07 02:40:12 +0100

    début d'implémentation de l'amélioration des usines

--- Modele/classe_TUsine.php
+++ Modele/classe_TUsine.php
@@ -56,8 +56,37 @@ protected function Afficher_rapport($Tvariables, $id_selectionné) {
 	<p>les usines peuvent elles assurer les besoins pour produire</p>
 
 	<h1>Am&eacute;lioration</h1>
-	<p>besoins</p>
-	<p>ordre am&eacute;lioration</p>
+	<table id="amélioration">
+	<thead>
+		<tr><th>Marchandise</th><th>Quantit&eacute;</th><th>stock</th><th>manque</th><th>PU</th><th>achat</th>		</tr>
+	</thead>
+	<tbody>
+	</tbody>
+<?php
+		$T_ligneBD = $this->InterrogerBD(
+			"SELECT CONCAT('\t\t<tr><td>',nom,'</td><td>',Qte,'</td><td>',stock,'</td><td>',manque,'</td><td>',PU,'</td><td>',achat,'</td></tr>\n') AS code
+			FROM (SELECT
+					marchandise.nom
+					,-ingredient.quantité * POWER(usine.niveau+1,2) AS Qte #consommation est négative
+					,entrepot.stock
+					,IF((SELECT Qte) > entrepot.stock,(SELECT Qte) - entrepot.stock,0) AS manque
+					,marchandise.cours_max AS PU
+					,(SELECT manque) * marchandise.cours_max AS achat
+				FROM usine
+				INNER JOIN type_usine ON usine.type_usine_ID = type_usine.ID
+				INNER JOIN recette ON type_usine.amelioration_ID = recette.ID
+				INNER JOIN ingredient ON ingredient.recette_ID = recette.ID
+				INNER JOIN marchandise ON ingredient.marchandise_ID = marchandise.ID
+				INNER JOIN entrepot ON entrepot.marchandise_ID = marchandise.ID
+				WHERE ingredient.nature = 0 AND #ingrédient de la recette 
+				usine.joueur_ID = :IDjoueur AND usine.ID = :ID) AS Amélioration # ne permet pas de connaitre le cout fixe car l'argent n'a pas d'entrepot"
+			,array(':IDjoueur'=>$_SESSION['IDjoueur'], ':ID'=>$_SESSION['ID']));
+		foreach($T_ligneBD as $ligne) echo $ligne['code'];
+?>
+	</table>
+	<p>frais de transport</p>
+	<p>co&ucirc;t total</p>
+	<p>ordre am&eacute;lioration et délai</p>
 <?php
 }
 }
