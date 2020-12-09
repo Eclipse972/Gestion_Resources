41cdaeff92da5e7c4d4fabbe0de8ab97d9c34eca
ChristopHe <christophe.hervi@free.fr>
2020-12-09 05:34:26 +0100

    ajout code erreur pour le formulaire

--- erreur.php
+++ erreur.php
@@ -4,6 +4,7 @@ $DICO = array(
 	// erreurs de mon application
 	0	=> 'Erreur inconnue',
 	1	=> 'Un probl&egrave;me est survenu lors de l&apos;envoi de votre message'."\n".'R&eacute;essayez plus tard!',
+	2	=> 'Un probl&egrave;me est survenu avec le formulaire',
 	// erreurs serveur
 	403	=> 'Acc&egrave;s interdit',
 	404	=> 'Cette page n&apos;existe pas',
