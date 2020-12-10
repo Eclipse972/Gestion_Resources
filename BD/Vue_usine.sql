d9cda8a94c757512c8dce1b5215c4bfd1ec427f7
ChristopHe <christophe.hervi@free.fr>
2020-12-10 15:21:31 +0100

    factorisation des liens

--- BD/Vue_usine.sql
+++ BD/Vue_usine.sql
@@ -2,9 +2,11 @@ CREATE VIEW Vue_usine AS
 SELECT 
 	type_usine.ID,
 	usine.joueur_ID AS IDjoueur,
+	CONCAT('<a href="?id=',CAST(type_usine.ID AS CHAR),'#selection">') AS lien_rapport, # utilisé 2 fois
+	CONCAT('<a href="formulaire.php?id=',CAST(type_usine.ID AS CHAR),'">') AS lien_formulaire, # idem
 	CONCAT(
-		'\t\t<td><p id="gauche"><a href="?id=',CAST(type_usine.ID AS CHAR),'#selection"><img src="Vue/images/',type_usine.image, '.png" alt ="',type_usine.nom,'"></a></p>\n',
-		'\t\t\t<a href="?id=',CAST(type_usine.ID AS CHAR),'#selection"><strong>',UCASE(LEFT(type_usine.nom,1)),SUBSTRING(type_usine.nom,2,LENGTH(type_usine.nom)),'</strong></a>\n\t\t\t<p id="petite_image">',
+		'\t\t<td><p id="gauche"',(SELECT lien_rapport),'<img src="Vue/images/',type_usine.image, '.png" alt ="',type_usine.nom,'"></a></p>\n\t\t\t',
+		(SELECT lien_rapport),'<strong>',UCASE(LEFT(type_usine.nom,1)),SUBSTRING(type_usine.nom,2,LENGTH(type_usine.nom)),'</strong></a>\n\t\t\t<p id="petite_image">',
 		(SELECT GROUP_CONCAT( # requête qui crée la recette
 					REPLACE(CAST(FORMAT(ABS(ingredient.quantité),0) AS CHAR),',',' '),
 					'\t<img src="Vue/images/',marchandise.image,'.png" alt ="',marchandise.nom,'">',
@@ -15,7 +17,8 @@ SELECT
 			INNER JOIN marchandise ON ingredient.marchandise_ID = marchandise.ID
 			WHERE ingredient.recette_ID = type_usine.production_ID
 		),
-		'</p>\n\t\t</td>\n\t\t<td><a href="formulaire.php?id=',CAST(type_usine.ID AS CHAR),'">',CAST(usine.niveau AS CHAR),'</a></td>\n',
+		'<br>',(SELECT lien_formulaire),'Temps de production restant:</a>',
+		'</p>\n\t\t</td>\n\t\t<td>',(SELECT lien_formulaire),CAST(usine.niveau AS CHAR),'</a></td>\n',
 		'\t\t<td>',REPLACE(CAST(FORMAT(type_usine.prod_niveau1*usine.niveau,0) AS CHAR),',',' '),' ',unites.nom,'/h</td>\n'
 	) AS code,
 	type_usine.nom AS nom_ligne
