385df2b7734a86932bcf18fc084bc5fa39120509
ChristopHe <christophe.hervi@free.fr>
2020-12-10 21:00:40 +0100

    ajout du lien vers le formulaire

--- BD/Vue_entrepot.sql
+++ BD/Vue_entrepot.sql
@@ -1,12 +1,13 @@
 CREATE VIEW Vue_entrepot AS
 SELECT 
-	entrepot.marchandise_ID AS ID
-	,entrepot.joueur_ID AS IDjoueur
-	,CONCAT(
+	entrepot.marchandise_ID AS ID,
+	entrepot.joueur_ID AS IDjoueur,
+	CONCAT('<a href="formulaire.php?id=',CAST(entrepot.marchandise_ID AS CHAR),'">') AS lien_formulaire,
+	CONCAT(
 		'\t\t<td><a href="?id=',entrepot.marchandise_ID,'#selection"><img src="Vue/images/',marchandise.image, '.png" alt ="',marchandise.nom,'"><strong>',
-		UCASE(LEFT(marchandise.nom,1)),SUBSTRING(marchandise.nom,2,LENGTH(marchandise.nom)),'</strong></a></td>\n',
-		'\t\t<td>',CAST(entrepot.niveau AS CHAR),'</td>\n',
-		'\t\t<td>',REPLACE(CAST(FORMAT(entrepot.stock,0) AS CHAR),',',' '),' ',unites.nom,'</td>\n'
+		UCASE(LEFT(marchandise.nom,1)),SUBSTRING(marchandise.nom,2,LENGTH(marchandise.nom)),'</strong></a></td>\n\t\t<td>',
+		(SELECT lien_formulaire),CAST(entrepot.niveau AS CHAR),'</a></td>\n\t\t<td>',
+		(SELECT lien_formulaire),CAST(entrepot.niveau AS CHAR),REPLACE(CAST(FORMAT(entrepot.stock,0) AS CHAR),',',' '),' ',unites.nom,'</a></td>\n'
 	) AS code
 FROM entrepot
 INNER JOIN marchandise ON entrepot.marchandise_ID = marchandise.ID
