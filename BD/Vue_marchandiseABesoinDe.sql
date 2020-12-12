e73c8eefd3b7642207348b56d427d70ac97b0a35
ChristopHe <christophe.hervi@free.fr>
2020-12-12 11:49:44 +0100

    permet de rajouter les euros dans les recettes d'extraction et de production

--- BD/Vue_marchandiseABesoinDe.sql
+++ BD/Vue_marchandiseABesoinDe.sql
@@ -9,5 +9,5 @@ INNER JOIN marchandise		ON ingredient.marchandise_ID	= marchandise.ID
 INNER JOIN nature_recette	ON recette.nature_ID			= nature_recette.ID
 INNER JOIN ingredient ingS	ON ingS.recette_ID		= recette.ID
 INNER JOIN marchandise marS	ON ingS.marchandise_ID	= marS.ID
-WHERE ingredient.nature = 2 AND ingS.nature = 0
+WHERE ingredient.nature = 2 AND ingS.nature < 2
 ORDER BY ingredient.marchandise_ID ASC
