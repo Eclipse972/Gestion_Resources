ab30762664a579c847f6d2557b1fc9ccbb15817d
ChristopHe <christophe.hervi@free.fr>
2020-12-13 09:29:56 +0100

    ajout des entrepots manquants

---
+++ table_entrepot.php
@@ -0,0 +1,15 @@
+<?php
+// remplissage de la table commerce
+try	{
+	include 'connexion.php'; // les variables de connexion sont définies dans ce script non suivi par git
+	$BD = new PDO($dsn, $utilisateur, $mdp); // On se connecte au serveur MySQL
+	$BD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$requete = $BD->prepare('INSERT INTO entrepot (marchandise_ID, joueur_ID, ordre_affichage) VALUES (:marchandise_ID, :IDJoueur, :place)');
+	for ($IDJoueur=1; $IDJoueur<3; $IDJoueur++)
+		for($marchandise_ID=39; $marchandise_ID<61; $marchandise_ID++)
+			$requete->execute(array(':IDJoueur'=>$IDJoueur, ':marchandise_ID'=>$marchandise_ID, ':place'=>$marchandise_ID-2));
+} catch (PDOException $e) {
+	exit('Erreur : '.$e->getMessage());
+}
+$BD = null; // on ferme la connexion
+echo "fin sans encombre";
