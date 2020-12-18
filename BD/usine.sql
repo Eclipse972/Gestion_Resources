31b458902cb0a1bf0d0cf5c2eaf7142bad7e9bf2
ChristopHe <christophe.hervi@free.fr>
2020-12-18 02:06:57 +0100

    ajout de la quantité en cours de production dans la table usine

--- BD/usine.sql
+++ BD/usine.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: gestion.resources.sql.free.fr
--- Généré le : Sam 12 Décembre 2020 à 06:34
+-- Généré le : Ven 18 Décembre 2020 à 02:05
 -- Version du serveur: 5.0.83
 -- Version de PHP: 5.3.9
 
@@ -29,6 +29,7 @@ CREATE TABLE IF NOT EXISTS `usine` (
   `joueur_ID` tinyint(3) unsigned NOT NULL,
   `type_usine_ID` tinyint(3) unsigned NOT NULL,
   `niveau` int(10) unsigned NOT NULL,
+  `prod_en_cours` int(10) unsigned NOT NULL default '1' COMMENT 'quantité en  cours de production',
   `date_fin_production` int(10) unsigned NOT NULL default '0' COMMENT 'format timestamp',
   `duree_prod_souhaitee` int(10) unsigned NOT NULL default '7200' COMMENT 'format timestamp',
   `ordre_affichage` tinyint(1) unsigned NOT NULL default '0',
@@ -39,29 +40,29 @@ CREATE TABLE IF NOT EXISTS `usine` (
 -- Contenu de la table `usine`
 --
 
-INSERT INTO `usine` (`joueur_ID`, `type_usine_ID`, `niveau`, `date_fin_production`, `duree_prod_souhaitee`, `ordre_affichage`) VALUES
-(1, 1, 51, 1608238441, 432000, 0),
-(1, 2, 26, 1607631435, 0, 1),
-(1, 3, 13, 1607811698, 7200, 2),
-(1, 4, 10, 1608789777, 7200, 3),
-(1, 5, 15, 1607829892, 7200, 4),
-(1, 6, 14, 1607398072, 4320, 6),
-(1, 7, 459, 1607605464, 8640, 7),
-(1, 8, 186, 1607886003, 8640, 8),
-(1, 9, 7, 1608246869, 8640, 9),
-(1, 10, 43, 1607891955, 8640, 10),
-(1, 11, 18, 1607408173, 8640, 11),
-(1, 12, 32, 1608149177, 8640, 12),
-(1, 13, 7, 1608108114, 8640, 13),
-(1, 14, 53, 1607528936, 4320, 0),
-(1, 15, 31, 1607372676, 2880, 0),
-(1, 16, 22, 1607669082, 8640, 5),
-(1, 17, 11, 1609205906, 8640, 0),
-(1, 18, 29, 1607826153, 8640, 0),
-(1, 19, 18, 1607567025, 8640, 0),
-(1, 20, 18, 1607567026, 7200, 0),
-(1, 21, 15, 1607779068, 8640, 0),
-(1, 22, 39, 1607779052, 8640, 0);
+INSERT INTO `usine` (`joueur_ID`, `type_usine_ID`, `niveau`, `prod_en_cours`, `date_fin_production`, `duree_prod_souhaitee`, `ordre_affichage`) VALUES
+(1, 1, 25, 1, 1608420571, 432000, 0),
+(1, 2, 39, 1, 1608260219, 0, 1),
+(1, 3, 32, 1, 1608346849, 7200, 2),
+(1, 4, 123, 1, 1608789777, 7200, 3),
+(1, 5, 22, 1, 1607829892, 7200, 4),
+(1, 6, 14, 1, 1607398072, 4320, 6),
+(1, 7, 333, 1, 1607605464, 8640, 7),
+(1, 8, 186, 1, 1607886003, 8640, 8),
+(1, 9, 11, 1, 1608246869, 8640, 9),
+(1, 10, 43, 1, 1607891955, 8640, 10),
+(1, 11, 18, 1, 1607408173, 8640, 11),
+(1, 12, 32, 1, 1608149177, 8640, 12),
+(1, 13, 9, 1, 1608108114, 8640, 13),
+(1, 14, 53, 1, 1607528936, 4320, 0),
+(1, 15, 31, 1, 1607372676, 2880, 0),
+(1, 16, 22, 1, 1607669082, 8640, 5),
+(1, 17, 11, 1, 1609205906, 8640, 0),
+(1, 18, 29, 1, 1607826153, 8640, 0),
+(1, 19, 18, 1, 1607567025, 8640, 0),
+(1, 20, 18, 1, 1607567026, 7200, 0),
+(1, 21, 15, 1, 1607779068, 8640, 0),
+(1, 22, 39, 1, 1607779052, 8640, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
