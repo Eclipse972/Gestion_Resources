97456aa765bc15a921fd9166fe88276be9695786
ChristopHe <christophe.hervi@free.fr>
2020-12-22 13:23:10 +0100

    début de factorisation vers la classe MAJLigne

--- Controleur/MAJUsine.php
+++ Controleur/MAJUsine.php
@@ -1,43 +1,31 @@
 <?php
 session_start();
 require'../Modele/classe_MAJLigne.php';
-$usine = new usineMAJ($_SESSION['IDjoueur'], $_GET['ID']);
+$usine = new usineMAJ($_SESSION['IDjoueur'], $_POST['ID']);
 
+$Tpost = $usine->FormaterParamètres($_POST);
+$ID			= $Tpost['ID'];
+$niveau		= $Tpost['niveau'];
+$production = $Tpost['production'];
+$jour		= $Tpost['jour'];
+$heure		= $Tpost['heure'];
+$minute = ($jour + $heure + $Tpost['minute'] == 0) ? -1 : $Tpost['minute']; // si durée nulle on met l'heure de fin de production dans le passé
 
-// dans un premier temps je fait confiance au filtrage du formulaire
-$ID			= $_POST['ID'];
-$niveau		= $_POST['niveau'];
-$production = $_POST['production'];
-$jour		= $_POST['jour'];
-$heure		= $_POST['heure'];
-$minute		= $_POST['minute'];
-if ($jour + $heure + $minute == 0) $minute = -1; // si durée nulle on met l'heure de fin de production dans le passé
+$listeDchamps	= "	niveau = :niveau,
+					prod_en_cours = :prod,
+					date_fin_production = UNIX_TIMESTAMP() + 1 + 60*:minute + 3600*:heure + 86400*:jour";
+$T_paramètres = array(
+	':IDjoueur'	=> $_SESSION['IDjoueur'],
+	':ID'		=> $ID,
+	':niveau'	=> $niveau,
+	':prod'		=> $production,
+	':jour'		=> $jour,
+	':heure'	=> $heure,
+	':minute'	=> $minute);
+
 ////////////////////////////
-//echo"$ID, $niveau, $production, $jour, $heure, $minute";exit;
+//print_r($T_paramètres);exit;
 ////////////////////////////
+$usine->MiseAjour($listeDchamps, $T_paramètres);
 
-$IDjoueur = $_SESSION['IDjoueur'];
-
-try	{
-	include '../connexion.php';
-	$BD = new PDO($dsn, $utilisateur, $mdp);
-	$BD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$requete = $BD->prepare('
-		UPDATE usine
-		SET niveau = :niveau,
-			prod_en_cours = :prod,
-			date_fin_production = UNIX_TIMESTAMP() + 1 + 60*:minute + 3600*:heure + 86400*:jour
-		WHERE usine.joueur_ID = :IDjoueur AND usine.type_usine_ID = :ID');
-	$requete->bindValue(':IDjoueur'	, $IDjoueur,	PDO::PARAM_INT);
-	$requete->bindValue(':ID'		, $ID,			PDO::PARAM_INT);
-	$requete->bindValue(':niveau'	, $niveau,		PDO::PARAM_INT);
-	$requete->bindValue(':prod'		, $production,	PDO::PARAM_INT);
-	$requete->bindValue(':jour'		, $jour,		PDO::PARAM_INT);
-	$requete->bindValue(':heure'	, $heure,		PDO::PARAM_INT);
-	$requete->bindValue(':minute'	, $minute,		PDO::PARAM_INT);
-	$requete->execute();
-} catch (PDOException $e) {
-	exit('Erreur : '.$e->getMessage()); // faire un meilleur traitement de l'erreur
-}
-$BD = null; // on ferme la connexion
 header("Location: ".$usine->PageDeRetour($ID));
--- Modele/classe_MAJLigne.php
+++ Modele/classe_MAJLigne.php
@@ -4,32 +4,36 @@ abstract class MAJOnglet {
 	protected $champAmodifier;
 	protected $nomChampID;	// nom du champ permettant d'identifier le type d'opjet
 	protected $IDjoueur;	// le joueur doit être connu lors de la construction
-	protected $type_objetID;
 	protected $onglet;
 
 abstract public function IDvalide($valeur);
 
-public function __construct($IDjoueur, $typeID) {
-	$this->IDjoueur = $IDjoueur;
-	$this->type_objetID = $typeID;
+public function FormaterParamètres($T_post) {
+	$Trésultat = [];
+	foreach($T_post as $clé => $valeur) $Trésultat[$clé] = (int)htmlspecialchars(stripslashes(trim($valeur)));
+	return $Trésultat;
 }
 
-protected function MAJchampTypeEntier($champ, $chaine) { // les paramètres sont des chaines transmises par javascript
-	if (($this->IDvalide($this->type_objetID))	// ID du type d'objet est correct. IDvalide() est une méthode abstraite
-	&& (preg_match('#^[0-9]+$#', $chaine)))	{	// la chaîne est un entier
-		$valeur = (int)$chaine;
-		try	{
-			include'../connexion.php'; // car appelé depuis le doossier Controleur
-			$BD = new PDO($dsn, $utilisateur, $mdp);
-			$BD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = "UPDATE {$this->table} SET {$champ} = :valeur WHERE {$this->table}.joueur_ID = :IDjoueur AND {$this->table}.{$this->nomChampID} = {$this->type_objetID}";
-			$requete = $BD->prepare($sql);
-			$requete->bindValue(':valeur',	$valeur,		PDO::PARAM_INT);
-			$requete->bindValue(':IDjoueur',$this->IDjoueur,PDO::PARAM_INT);
-			$requete->execute();
-		} catch (PDOException $e) { exit('Erreur MAJchampTypeEntier: '.$e->getMessage()); }
-		$BD = null;
-	}
+public function __construct($IDjoueur) {
+	$this->IDjoueur = $IDjoueur;
+}
+
+public function MiseAjour($listeDchamps, $T_paramètres) { // les paramètres sont des chaines transmises par javascript
+	try	{
+		include'../connexion.php'; // car appelé depuis le doossier Controleur
+		$BD = new PDO($dsn, $utilisateur, $mdp);
+		$BD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "UPDATE {$this->table}
+				SET {$listeDchamps}
+				WHERE {$this->table}.joueur_ID = :IDjoueur AND {$this->table}.{$this->nomChampID} = :ID";
+		///////////////////////////
+		//echo"requête = $sql";exit;
+		///////////////////////////
+		$requete = $BD->prepare($sql);
+		foreach($T_paramètres as $clé => $valeur) { $requete->bindValue($clé, $valeur, PDO::PARAM_INT); }
+		$requete->execute();
+	} catch (PDOException $e) { exit('Erreur MAJ ligne de tableau: '.$e->getMessage()); }
+	$BD = null;
 }
 
 public function PageDeRetour($ID) {
@@ -53,16 +57,6 @@ public function IDvalide($valeur) {
 	return (($valeur > 0) && ($valeur <= 22));
 }
 
-public function Niveau($chaine) { $this->MAJchampTypeEntier('niveau', $chaine); }
-
-public function Production($chaine) { $this->MAJchampTypeEntier('prod_en_cours', $chaine); }
-
-public function TempsProdRestant($chaine) { // temps au format j-HH-mm
-	if (preg_match('#^[0-9]+-[0-2][0-9]-[0-5][0-9]$#', $chaine)) {
-		list($jour, $heure, $minute) = preg_split('/-/',$chaine);
-		$this->MAJchampTypeEntier('date_fin_production', strval(time() + 60*(int)$minute + 3600*(int)$heure + 86400*(int)$jour+9));
-	} // sinon on ne fait rien
-}
 }
 ///////////////////////////////////////////////////////////////////////
 class mineMAJ extends MAJOnglet {
@@ -73,6 +67,8 @@ public function __construct($IDjoueur, $type_mineID) {
 	$this->nomChampID = 'type_mine_ID';
 	$this->onglet = 'mines';
 }
+
+public function FormaterParamètres() {}
 
 public function IDvalide($valeur) {
 	$valeur = (int)$valeur;
@@ -96,6 +92,8 @@ public function __construct($IDjoueur, $type_mineID) {
 	$this->nomChampID = 'marchandise_ID';
 	$this->onglet = 'entrepots';
 }
+
+public function FormaterParamètres() {}
 
 public function IDvalide($valeur) {
 	$valeur = (int)$valeur;
