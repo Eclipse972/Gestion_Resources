fd1fc665c3742d9b74c4e132cb277373e3ecbd9f
ChristopHe <christophe.hervi@free.fr>
2020-12-23 01:39:06 +0100

    correction des liens dans les vues

--- BD/Vue_commerce.sql
+++ BD/Vue_commerce.sql
@@ -1,9 +1,9 @@
 CREATE VIEW Vue_commerce AS
-SELECT 
+SELECT
 	commerce.joueur_ID AS IDjoueur,
 	marchandise.ID,
 	CONCAT(
-		'\t\t<td><a href="?id=',ID,'#selection"><img src="Vue/images/',marchandise.image, '.png" alt ="',marchandise.nom,'"><strong>',
+		'\t\t<td><a href="?id=',ID,'#',ID,'"><img src="Vue/images/',marchandise.image, '.png" alt ="',marchandise.nom,'"><strong>',
 		UCASE(LEFT(nom,1)),SUBSTRING(marchandise.nom,2,LENGTH(marchandise.nom)),'</strong></a></td>\n',
 		'\t\t<td>',REPLACE(FORMAT(marchandise.cours_ki ,0),',', ' '),'&euro;</td>\n',
 		'\t\t<td>',REPLACE(FORMAT(marchandise.cours_max ,0),',', ' '),'&euro;</td>\n'
--- BD/Vue_entrepot.sql
+++ BD/Vue_entrepot.sql
@@ -3,16 +3,19 @@ SELECT
 	entrepot.marchandise_ID AS ID,
 	entrepot.joueur_ID AS IDjoueur,
 	entrepot.niveau * entrepot.niveau * 5000 AS capacité,
+	CONCAT('<a href="#" onclick="OuvrirFormulaireMAJ(',entrepot.marchandise_ID,',',
+		'''',marchandise.image,''',',
+		''''',',			#--'''',marchandise.nom,''',', provoque un bug avec les noms contenant une apostrophe
+		entrepot.niveau,',',
+		entrepot.stock,')">')	AS lien_MAJ,
 	CONCAT(
-		'\t\t<td><a href="?id=',entrepot.marchandise_ID,'#selection"><img src="Vue/images/',marchandise.image, '.png" alt ="',marchandise.nom,'"><strong>',
+		'\t\t<td><a href="?id=',entrepot.marchandise_ID,'#',entrepot.marchandise_ID,'"><img src="Vue/images/',marchandise.image, '.png" alt ="',marchandise.nom,'"><strong>',
 		UCASE(LEFT(marchandise.nom,1)),SUBSTRING(marchandise.nom,2,LENGTH(marchandise.nom)),'</strong></a>',
 		IF((SELECT capacité) >= entrepot.stock, '', '<span style="background-color:red"> Niveau (capacit&eacute;) et stock incoh&eacute;rents </span>'),
 		'</td>\n\t\t<td>',
-		'<a href ="#" onclick="Niveau(',CAST(marchandise.ID AS CHAR),',',CAST(entrepot.niveau AS CHAR),')">',
-		CAST(entrepot.niveau AS CHAR),'</a></td>\n\t\t<td>',
+		(SELECT lien_MAJ),CAST(entrepot.niveau AS CHAR),'</a></td>\n\t\t<td>',
 		REPLACE(CAST(FORMAT(entrepot.niveau * entrepot.niveau * 5000,0) AS CHAR),',',' '),' ',unites.nom,'</td>\n\t\t<td>',
-		'<a href ="#" onclick="Stock(',CAST(marchandise.ID AS CHAR),',',CAST(entrepot.stock AS CHAR),')">',
-		REPLACE(CAST(FORMAT(entrepot.stock,0) AS CHAR),',',' '),' ',unites.nom,'</a></td>\n'
+		(SELECT lien_MAJ),REPLACE(CAST(FORMAT(entrepot.stock,0) AS CHAR),',',' '),' ',unites.nom,'</a></td>\n'
 	) AS code
 FROM entrepot
 INNER JOIN marchandise ON entrepot.marchandise_ID = marchandise.ID
