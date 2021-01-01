fb12643b5a45fd3c536b529c8cf99c12159fb290
ChristopHe <christophe.hervi@free.fr>
2021-01-01 05:50:44 +0100

    taille des images dans le rapport

--- BD/Vue_recette.sql
+++ BD/Vue_recette.sql
@@ -2,7 +2,7 @@ CREATE VIEW Vue_recette AS
 SELECT type_usine.ID AS ID,
 	GROUP_CONCAT(
 		IF(ingredient.quantité < 0, -ingredient.quantité, ingredient.quantité)
-		,'<img src="Vue/images/',marchandise.image,'.png" alt="',marchandise.nom,'">"'
+		,'<img src="Vue/images/',marchandise.image,'.png" alt="',marchandise.nom,'">'
 		,marchandise.nom
 		,CASE ingredient.nature
 			WHEN 0 THEN ' +'
