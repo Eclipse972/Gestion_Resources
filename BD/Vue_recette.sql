5f2f1350dc05a624676c2e9e5f4c85130f24728d
ChristopHe <christophe.hervi@free.fr>
2021-01-02 00:19:02 +0100

    suppression du nom des ingrédients dans la recette

--- BD/Vue_recette.sql
+++ BD/Vue_recette.sql
@@ -3,14 +3,14 @@ SELECT type_usine.ID AS ID,
 	GROUP_CONCAT(
 		IF(ingredient.quantité < 0, -ingredient.quantité, ingredient.quantité)
 		,'<img src="Vue/images/',marchandise.image,'.png" alt="',marchandise.nom,'">'
-		,marchandise.nom
 		,CASE ingredient.nature
 			WHEN 0 THEN ' +'
 			WHEN 1 THEN ' ->'
 			ELSE ''
 		END
-	ORDER BY ingredient.nature ASC
-	SEPARATOR ' ') AS formule
+		ORDER BY ingredient.nature ASC
+		SEPARATOR ' '
+	) AS formule
 FROM ingredient
 INNER JOIN marchandise ON ingredient.marchandise_ID = marchandise.ID
 INNER JOIN type_usine ON type_usine.production_ID = ingredient.recette_ID
