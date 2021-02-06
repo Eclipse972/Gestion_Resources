ab9e09b097ac2212ba0d8fac2d31e73bc378492d
ChristopHe <christophe.hervi@free.fr>
2021-02-06 15:20:30 +0100

    ébauche de procédure stockée pour la création du lien pour afficher le détail de la ligne quelque soit l'onglet

---
+++ BD/Procedure_Rapport.sql
@@ -0,0 +1,14 @@
+DROP PROCEDURE Rapport;
+DELIMITER |
+CREATE PROCEDURE Rapport(IN onglet INT, IN ID INT, IN nom TEXT, IN IDimage, OUT lien TEXT)
+BEGIN
+	SELECT CONCAT(
+		#-- lien
+		'<a href="/?onglet=',onglet,'&ligne=',ID,'#',ID,'" title="afficher d&eacute;tail">',
+		#-- image à gauche
+		'<span class="gauche"><img src="https://www.resources-game.ch/images/appimages/res',IDimage,'.png" alt ="',nom,'"></span>',
+		#-- texte
+		'<strong>',UCASE(LEFT(nom,1)),SUBSTRING(nom,2,LENGTH(nom)),'</strong></a>\n'
+	) INTO lien
+END|
+DELIMITER ;
