aa99acbb7b28a0763c25347a7aa63fe008897454
ChristopHe <christophe.hervi@free.fr>
2021-02-06 15:24:06 +0100

    commentaire

--- BD/Procedure_Rapport.sql
+++ BD/Procedure_Rapport.sql
@@ -3,11 +3,11 @@ DELIMITER |
 CREATE PROCEDURE Rapport(IN onglet INT, IN ID INT, IN nom TEXT, IN IDimage, OUT lien TEXT)
 BEGIN
 	SELECT CONCAT(
-		#-- lien
+		#-- début lien
 		'<a href="/?onglet=',onglet,'&ligne=',ID,'#',ID,'" title="afficher d&eacute;tail">',
 		#-- image à gauche
 		'<span class="gauche"><img src="https://www.resources-game.ch/images/appimages/res',IDimage,'.png" alt ="',nom,'"></span>',
-		#-- texte
+		#-- nom
 		'<strong>',UCASE(LEFT(nom,1)),SUBSTRING(nom,2,LENGTH(nom)),'</strong></a>\n'
 	) INTO lien
 END|
