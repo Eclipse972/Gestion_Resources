dc4a0c347766745b9233dee4ef157e8c612a16aa
ChristopHe <christophe.hervi@free.fr>
2021-02-07 01:05:43 +0100

    titre de lien pour les mines

--- BD/Vue_mine.sql
+++ BD/Vue_mine.sql
@@ -8,16 +8,16 @@ SELECT
 	#-- code HTML
 	CONCAT('<td>',
 		#-- rapport
-		'<a href="/?onglet=2&ligne=',type_mine.ID,'#',type_mine.ID,'"><img src="https://www.resources-game.ch/images/appimages/res',marchandise.IDimage, '.png" alt ="',type_mine.nom,'"><strong>',
+		'<a href="/?onglet=2&ligne=',type_mine.ID,'#',type_mine.ID,'" title="afficher d&eacute;tail"><img src="https://www.resources-game.ch/images/appimages/res',marchandise.IDimage, '.png" alt ="',type_mine.nom,'"><strong>',
 		UCASE(LEFT(type_mine.nom,1)),SUBSTRING(type_mine.nom,2,LENGTH(type_mine.nom)),'</strong></a></td>\n\t\t<td>',
 		#-- état
-		(SELECT lien_MAJ),'1#',type_mine.ID,'">',mine.etat,'%</a></td>\n\t\t<td>',
+		(SELECT lien_MAJ),'1#',type_mine.ID,'" title="modifier état la mine"',mine.etat,'%</a></td>\n\t\t<td>',
 		#-- nombre
-		(SELECT lien_MAJ),'0#',type_mine.ID,'">',mine.nombre,'</a></td>\n\t\t<td>',
+		(SELECT lien_MAJ),'0#',type_mine.ID,'" title="modifier le nombre de mine">',mine.nombre,'</a></td>\n\t\t<td>',
 		#-- production actuelle
 		REPLACE(REPLACE(FORMAT(mine.prod_max*mine.etat/100,1),',',' '),'.',','),' ',unites.nom,'/h</td>\n\t\t<td>',
 		#-- production max
-		(SELECT lien_MAJ),'2#',type_mine.ID,'">',REPLACE(REPLACE(FORMAT(mine.prod_max,0),',',' '),'.',','),' ',unites.nom,'/h</a></td>\n'
+		(SELECT lien_MAJ),'2#',type_mine.ID,'" title="modifier la production maximale de la mine">',REPLACE(REPLACE(FORMAT(mine.prod_max,0),',',' '),'.',','),' ',unites.nom,'/h</a></td>\n'
 	) AS code,
 	type_mine.nom AS nom_ligne
 FROM mine
